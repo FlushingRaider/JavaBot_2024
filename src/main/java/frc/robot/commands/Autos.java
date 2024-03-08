c49554572f8925ad9178028714e00542d8125ea6
Chris B <115883974+ChrisB5561@users.noreply.github.com>
2024-03-08 12:13:49 -0500

    autos.java fixed

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -10,11 +10,11 @@ import edu.wpi.first.wpilibj2.command.Commands;
 
 public final class Autos {
   /** Example static factory for an autonomous command. */
-  public static Command exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
-  }
+  // public static Command exampleAuto(ExampleSubsystem subsystem) {
+  //   return Commands.sequence(subsystem.exampleMethodCommand(), new (subsystem));
+  // }
 
-  private Autos() {
-    throw new UnsupportedOperationException("This is a utility class!");
-  }
+  // private Autos() {
+  //   throw new UnsupportedOperationException("This is a utility class!");
+  // }
 }
