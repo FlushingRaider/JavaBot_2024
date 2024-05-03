e3d2fcc1f3d1f0dca6c87865472a08ac1467d46d
Chris B <115883974+Darkwolf7898@users.noreply.github.com>
2024-05-03 19:42:51 -0400

    path, auto, logger [sim works]

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,10 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
+import org.littletonrobotics.urcl.URCL;
+import com.ctre.phoenix6.SignalLogger;
+import edu.wpi.first.wpilibj.DriverStation; 
+import edu.wpi.first.wpilibj.DataLogManager;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -25,8 +28,17 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
+  
+    DataLogManager.start();
+    DriverStation.startDataLog(DataLogManager.getLog());
+
+/*NOTE: These logging commands are for the CTRE Tuner X */
+    SignalLogger.setPath("/U/logs");
+    SignalLogger.start();
+/*END */
+    DataLogManager.start();
+    URCL.start();
+
     m_robotContainer = new RobotContainer();
   }
 
